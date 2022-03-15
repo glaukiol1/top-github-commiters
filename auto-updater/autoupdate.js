f893f633d8cf8b1ba1bc8998ae851c8bac9b8fda
glaukiol1 <glaukiol1@gmail.com>
2022-03-15 15:42:53 +0100

    this is a test by autoupdate.js

--- auto-updater/autoupdate.js
+++ auto-updater/autoupdate.js
@@ -18,3 +18,57 @@ const countries = [
     ["argentina"],
     ["addis+ababa", "ethiopia"]
 ]
+
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const getCountryList = require('../requesters/request_country');
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+
+const run = async (country) => {
+    return new Promise(async(resolve,reject)=> {
+        console.log(`Starting with country ${country}; waiting 5 seconds before start...\n\n\n`)
+        await sleep(5000)
+        await getCountryList(country, -1, process.argv[2])
+        .then(list=>{
+            var lowest_follower_amount = 100*100;
+            list.list.forEach(s=>{
+                if (s.followers.totalCount < lowest_follower_amount) {lowest_follower_amount = s.followers.totalCount};
+            })
+            writeToFile(makeMarkdown(list.list,country,lowest_follower_amount), country)
+            console.log(`Done with country ${country}, please check if the corresponding file at output/${country}.md has been updated`)
+            resolve()
+        })
+        .catch(error => {
+            console.error("Promise Error: "+error)
+            reject(error)
+        })
+    })
+    
+}
+
+
+const writeToFile = (data,country) => {
+    try {
+        fs.writeFileSync(path.join(__dirname, `./output/${country}.md`), data, {encoding: 'utf-8'})
+    } catch (error) {
+        fs.writeFileSync(path.join(__dirname, `../output/${country}.md`), data, {encoding: 'utf-8'})
+    }
+}
+
+async function main() {
+    for (const country of countries) {
+        await run(country)
+    }
+}
+
+// main()
+exec("git add .")
+exec(`git commit -m "this is a test by autoupdate.js"`)
+exec("git push")
